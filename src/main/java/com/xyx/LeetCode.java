cf5f47d189520c31431059a2ac1d51974197266f
XYX <brdsworld@gmail.com>
2023-03-04 22:22:47 +0800

    提交

---
+++ src/main/java/com/xyx/LeetCode.java
@@ -0,0 +1,22 @@
+package com.xyx;
+
+public class LeetCode {
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1,1};
+        int[] result = runningSum(nums);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+    public static int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        int temp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            temp += nums[i];
+            result[i] = temp;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/xyx/LeetCode0301.java
@@ -0,0 +1,35 @@
+package com.xyx;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class LeetCode0301 {
+    public static int maximumWealth(int[][] accounts) {
+        /*int[] sumArr = new int[accounts.length];
+        for (int i = 0; i < accounts.length; i++) {
+            int[] newByte = accounts[i];
+            int sum = 0;
+            for (int j = 0; j < newByte.length; j++) {
+                sum += newByte[j];
+            }
+            sumArr[i] = sum;
+        }
+        Arrays.sort(sumArr);
+
+        //官方回答
+        return sumArr[sumArr.length-1];*/
+        /*int maxWealth = Integer.MIN_VALUE;
+        for (int[] account : accounts) {
+            maxWealth = Math.max(maxWealth, Arrays.stream(account).sum());
+        }
+        return maxWealth;*/
+
+        return Arrays.stream(accounts).map(Arrays::stream).mapToInt(IntStream::sum).reduce(0, Math::max);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{3,7,1},{3,7,6},{3,7,3}};
+        int abc = maximumWealth(arr);
+        System.out.println(abc);
+    }
+}
