81460943c98a04f9cd913c44878839f4413da253
Xu Yixin <xu.yixin@ustcinfo.com>
2023-03-30 20:55:09 +0800

    提交

---
+++ src/main/java/com/xyx/leetcode/LeetCode0330.java
@@ -0,0 +1,90 @@
+package com.xyx.leetcode;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class LeetCode0330 {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+    /*
+    * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+    * */
+    HashSet<String> set = new HashSet();
+    public static int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        int num = 0 ;
+        for (int i = length-1; i > 0; i--) {
+            for (int j = i; j > 0 ; j--) {
+                String newStr = s.substring(j-1,i);
+                newStr = deduplication(newStr); // 去重
+                if (newStr.equals(s.substring(j-1,i))){
+                    num = newStr.length();
+                }
+            }
+        }
+        return num;
+    }
+    // 去重自己写的
+    public static String deduplication (String s) {
+        char[] array = s.toCharArray();
+        HashSet newSet = new HashSet<>();
+        for(char newChar : array){
+            newSet.add(newChar);
+        }
+        StringBuffer buffer = new StringBuffer();
+        Iterator iterator = newSet.iterator();
+        while (iterator.hasNext()){
+            buffer.append(iterator.next());
+        }
+        return buffer.toString();
+    }
+
+    //传参：str="qw23eqr123e4tt"
+    public String ARFA(String str) {
+        //创建一个空字符串用于接收去重后的字符串
+        String arfa="";
+        //遍历字符串str
+        for (int i = 0; i < str.length(); i++) {
+            //获取字符串中下标为i的字符（char类型）
+            char ch = str.charAt(i);
+            //如果字符串没有重复，即：每个字符的下标都会等价于该字符第一次出现时的下标
+            //判断字符串第一次出现ch字符时的下标是否等于i
+            if (str.indexOf(ch)==i) {
+                //将ch字符转化为String类型，并添加到字符串arfa的末尾
+                arfa=arfa.concat(String.valueOf(ch));
+            }
+        }
+        return arfa;
+    }
+//去重结果：qw23er14t
+
+    public String ARFA2(String str) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i))==i) {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     1.将传参str转化为StringBuffer类型
+     2.调用reverse()可以将StringBuffer类型sb实现倒序
+     3.调用toString()转化为String类型可以调用replaceAll(,)方法
+     4.replaceAll("(.)(?=.*\\1)", "")方法可以实现：
+     从前往后遍历，某字符出现的第二次，则第一出现的字符会被删除
+     5.再次将String类型rs转化为StringBuffer类型，并调用reverse()方法
+     实现倒序，调用toString()方法转化为String类型
+     **/
+//传参：str="qw23eqr123e4tt"
+    public String ARFA3(String str) {
+        StringBuffer sb = new StringBuffer(str);
+        String rs = sb.reverse().toString().replaceAll("(.)(?=.*\\1)", "");
+        StringBuffer out = new StringBuffer(rs);
+        return out.reverse().toString();
+    }
+    //去重结果：qw23er14t
+
+}
