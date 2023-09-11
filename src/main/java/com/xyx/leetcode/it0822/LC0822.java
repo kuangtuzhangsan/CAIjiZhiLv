66074801d957fe4260630e1600bba423bfceed42
xyx <xu.yixin@ustcinfo.com>
2023-09-11 10:23:16 +0800

    0822完成

--- src/main/java/com/xyx/leetcode/it0822/LC0822.java
+++ src/main/java/com/xyx/leetcode/it0822/LC0822.java
@@ -10,25 +10,44 @@ public class LC0822 {
      * @param s "caaaa" "aba"
      * @return
      */
-    public static String longestPalindrome(String s){
-        // 放置一个分割线，分割线从索引坐标1开始
-        char[] arr = s.toCharArray();
-        if (arr.length == 1){
+    public static String longestPalindrome(String s) {
+        if (s.length() == 1){
             return s;
         }
+        char[] arr = s.toCharArray();
+//        String res = new String(arr,startIndex,endIndex-startIndex+1);
+        String res = String.valueOf(arr[0]);
         int max = 1;
-        int startIndex = 0;
-        int endIndex  = 0;
         // 左边最大索引长度是索引，右边最大索引长度是 lenth-索引-1
-        for (int i = 1; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length; i++) {
+            // 从第一个索引开始，先向右，
+            for (int j = 1; i+j < arr.length && i-j > -1; j++) {
+                if (arr[i-j] == arr[i+j] ){
+                    if ((2*j + 1) > max){
+                        max = 2*j + 1;
+                        res = s.substring(i-j,i+j+1);
+                    }
+                }else {
+                    break;
+                }
+            }
 
+            for (int j = 0; i+j+1 < arr.length && i-j > -1; j++) {
+                if (arr[i-j] == arr[i+j+1]  ){
+                    if (2 * j+2 > max){
+                        max = 2 * j+2;
+                        res = s.substring(i-j,i+j+2);
+                    }
+                }else {
+                    break;
+                }
+            }
         }
-        String res = new String(arr,startIndex,endIndex-startIndex+1);
         return res;
     }
 
     public static void main(String[] args) throws ParseException {
-        String res = longestPalindrome("babad");
+        String res = longestPalindrome("aaaaaaababad");// ac, a
         System.out.println(res);
     }
 }
